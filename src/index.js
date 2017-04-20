61e93e2bd592cdc69e6d90f24e1fe5b4dd958a82
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-20 05:06:33 +0100

    Organise routes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 // Libs
 import React from 'react';
 import { render } from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
 
 // CSS 
 import './css/style.css';
 
-// Components
-import App from './components/App';
-import Home from './components/Home';
-import Courses from './components/Courses';
-import Teachers from './components/Teachers';
-import About from './components/About';
+// Routes
+import routes from './router';
 
 // Render
-render((
-  <Router>
-    <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/teachers" component={Teachers} />
-      <Route path="/courses" component={Courses} />
-    </div>
-  </Router>
-), document.getElementById('root'));
+render(routes, document.getElementById('root'));
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
+import About from './components/About';
+
+// Routes
+const routes = (
+  <Router>
+    <div>
+      <App>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/teachers" component={Teachers} />
+        <Route path="/courses" component={Courses} />
+      </App>
+    </div>
+  </Router>
+);
+
+export default routes;

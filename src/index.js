61bfc166da80169a2da177af8cd5d233b10dd22a
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-20 04:28:19 +0100

    Add react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 // Libs
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 // CSS 
 import './css/style.css';
 
 // Components
 import App from './components/App';
+import Home from './components/Home';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
+import About from './components/About';
 
 // Render
-render(
-  <App />,
-  document.getElementById('root')
-);
+render((
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </div>
+  </Router>
+), document.getElementById('root'));

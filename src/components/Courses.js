63d766bc4e6a56584f2bc3e42e9d95e046ea2509
anoopmundathan <anoop.mundathan@gmail.com>
2017-04-22 21:25:02 +0100

    Nested routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,26 @@
 import React, {Component} from 'react';
-import { NavLink } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
+
+import Course from './courses/Course';
+import CSS from './courses/CSS';
+import HTML from './courses/HTML';
+import JavaScript from './courses/JavaScript';
 
 class Courses extends Component {
   render() {
     return (
       <div className="main-content courses">
         <div className="course-header group">
-          <h2>Courses</h2> 
+          <h2>Courses</h2>
           <ul className="course-nav">
             <li><NavLink to="/courses/html">HTML</NavLink></li>
             <li><NavLink to="/courses/css">CSS</NavLink></li>
             <li><NavLink to="/courses/javascript">JavaScript</NavLink></li>
           </ul>
         </div>
+        <Route path="/courses/html" component={HTML}/>
+        <Route path="/courses/css" component={CSS}/>
+        <Route path="/courses/javascript" component={JavaScript}/>
       </div>
     );
   }
